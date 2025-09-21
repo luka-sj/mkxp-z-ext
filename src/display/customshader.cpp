8b44eddafe81821210dc7dffa31e72ce96d4f41a
Luka S.J <me@luka-sj.com>
2025-09-21 21:58:38 +0200

    Remove redundant function

--- src/display/customshader.cpp
+++ src/display/customshader.cpp
@@ -161,22 +161,6 @@ void CustomShader::setTranslation(const Vec2i &value)
     gl.Uniform2f(u_translation, value.x, value.y);
 }
 
-void CustomShader::releaseResources() {
-    GFX_LOCK;
-
-    if (program) {
-        gl.DeleteProgram(program);
-    }
-
-    if (vertShader) {
-        gl.DeleteShader(vertShader);
-    }
-
-    if (fragShader) {
-        gl.DeleteShader(fragShader);
-    }
-}
-
 void CustomShader::applyViewportProj()
 {
     if (disposed || u_projMat == -1) return;
